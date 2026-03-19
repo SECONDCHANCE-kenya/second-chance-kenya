aa12a9876830a68521d93cbda4ee4c25aced8fb3
SECONDCHANCE-kenya <peter.njoroge.njeri@gmail.com>
2026-03-19 19:26:53 +0100

    Refactor product card creation in script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 fetch('bags.json')
-.then(res => res.json())
-.then(data => {
-const container = document.getElementById('products');
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('products');
 
-data.forEach(bag => {
-const card = document.createElement('div');
-card.className = 'card';
+    data.forEach(bag => {
+      const div = document.createElement('div');
+      div.className = 'card';
 
-card.innerHTML = `
-<img src="${bag.image}">
-<div class="card-content">
-<h3>${bag.name}</h3>
-<p class="price">KSh ${bag.price}</p>
-</div>
-`;
+      div.innerHTML = `
+        <img src="${bag.image}">
+        <div class="card-content">
+          <h3>${bag.name}</h3>
+          <p class="price">KSh ${bag.price}</p>
+        </div>
+      `;
 
-container.appendChild(card);
-});
-});
+      container.appendChild(div);
+    });
+  });
